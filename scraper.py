c4e7351ec0817a9f571a4f7141a21697a1d072ac
Bhavanvir Rai <bhavanvir.r@gmail.com>
2022-09-22 11:21:02 -0700

    ♻️ Refactored code structure, and output.

--- scraper.py
+++ scraper.py
@@ -3,14 +3,12 @@ import requests
 from bs4 import BeautifulSoup
 
 # Math
-import math
 import statistics
 
 # Currency Conversion
 from currency_converter import CurrencyConverter
 
 # Sentiment Analysis
-import nltk
 #nltk.download()
 import nltk.corpus
 #nltk.download('stopwords')
@@ -32,7 +30,7 @@ def sentiment_analysis(text):
     if sentiment["compound"] <= -0.05:
         rating -= rating * sentiment["neg"]
 
-    return stars(rating)
+    return truncate_rating(rating)
 
 def clean_text(text):
     tokenizer = RegexpTokenizer('\w+|\$[\d\.]+|http\S+')
@@ -46,6 +44,33 @@ def clean_text(text):
     lemmatized = [lemmatizer.lemmatize(word) for word in filtered]
     
     return " ".join(lemmatized)
+
+def price_difference_rating(initial, final):
+    value = 100 - (initial / (final + initial)) * 100
+    rating = (value + 50) / 20
+
+    return truncate_rating(rating)
+
+def truncate_rating(rating):
+    if rating >= 5.0:
+        rating = 5.0
+    
+    return rating
+
+def stars(rating):
+    pure_rating = int(rating)
+    decimal = rating - pure_rating
+    score = "🌕" * pure_rating
+
+    if decimal > 0.75:
+        score += "🌕"
+    elif decimal > 0.25:
+        score += "🌗"
+
+    if len(score) < 5:
+        score += "🌑" * (5 - len(score))
+    
+    return score
 
 def get_listing_title(soup):
     title = soup.find("meta", {"name": "DC.title"})
@@ -64,30 +89,6 @@ def get_listing_price(soup):
     price = [str(span.text) for span in spans if "$" in span.text][0]
 
     return price
-
-def stars(rating):
-    if rating >= 5.0:
-        rating = 5.0
-
-    pure_rating = int(rating)
-    decimal = rating - pure_rating
-    score = "🌕" * pure_rating
-
-    if decimal > 0.75:
-        score += "🌕"
-    elif decimal > 0.25:
-        score += "🌗"
-
-    if len(score) < 5:
-        score += "🌑" * (5 - len(score))
-    
-    return score
-
-def price_rating(initial, final):
-    value = 100 - (initial / (final + initial)) * 100
-    rating = (value + 50) / 20
-
-    return stars(rating)
 
 def create_soup(url, headers):
     response = requests.get(url, headers=headers)
@@ -143,7 +144,7 @@ def main():
     shortened_url = re.search(r".*[0-9]", url).group(0)
     mobile_url = shortened_url.replace("www", "m")
 
-    sentiment = sentiment_analysis(get_listing_description(create_soup(url, headers=None)))
+    sentiment_rating = sentiment_analysis(get_listing_description(create_soup(url, headers=None)))
     title = get_listing_title(create_soup(url, headers=None))
 
     initial_price = int(re.sub("[\$,]", "", get_listing_price(create_soup(mobile_url, headers=None))))
@@ -153,10 +154,14 @@ def main():
     upper_bound = abs(median + deviation)
     bound_average = statistics.mean([lower_bound, upper_bound])
 
-    print("\nProduct: {} \nPrice: ${:,.2f}\n".format(title, initial_price))
-    print("Description rating: {}".format(sentiment))
-    print("Price rating: {}".format(price_rating(initial_price, bound_average)))
-    print("Price range of similar products: ${:,.2f} - ${:,.2f}".format(lower_bound, upper_bound))
+    price_rating = price_difference_rating(initial_price, bound_average)
+    average_rating = statistics.mean([sentiment_rating, price_rating])
+
+    print("\nProduct: {} \nPrice: ${:,.2f}".format(title, initial_price))
+    print("Price range of similar products: ${:,.2f} - ${:,.2f}\n".format(lower_bound, upper_bound))
+    print("Description rating: {}".format(stars(sentiment_rating)))
+    print("Price rating: {}".format(stars(price_rating)))
+    print("Overall rating: {}".format(stars(average_rating)))
 
 if __name__ == "__main__":
     main()

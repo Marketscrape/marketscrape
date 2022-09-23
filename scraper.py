26e05946abcd658473d17e1ca41ef3e121010c0e
Bhavanvir Rai <bhavanvir.r@gmail.com>
2022-09-22 21:10:46 -0700

    🚑 "#" or "&" tags in a search title would break the application.

--- scraper.py
+++ scraper.py
@@ -103,6 +103,9 @@ def convert_currency(price, base_currency, target_currency):
     return price
 
 def find_product_prices(title):
+    title = re.sub(r"#", "%2", title)
+    title = re.sub(r"&", "%26", title)
+    
     headers = { 
         "User-Agent":
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102 Safari/537.36 Edge/18.19582"

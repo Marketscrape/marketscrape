d78dd6023c4c78429575d187c9c4b1653f8f46aa
Alex Holland <64433812+alexwholland@users.noreply.github.com>
2023-01-01 19:13:01 -0800

    Operation SK

--- marketscrape/marketscrape/settings.py
+++ marketscrape/marketscrape/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@p4$*8)=h7vd43=gg!vp+)+t&+^_75_(r9fsx29241=99%(*!c'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

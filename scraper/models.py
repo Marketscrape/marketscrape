8e9c21292c4d59c7540f87fbd4f492e0d4218779
alexwholland <alexwholland@gmail.com>
2023-03-25 21:05:50 -0700

    Added padding on main page and missing model content

--- scraper/models.py
+++ scraper/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    title = models.CharField(max_length=1000)
+    rating = models.DecimalField(max_digits=8, decimal_places=1)
+    url = models.CharField(max_length=1000)
+    image = models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.title
+
+
+
+

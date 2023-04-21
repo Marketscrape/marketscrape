491760059609e5bbbf03d66d97b2352d9b25d499
alexwholland <alexwholland@gmail.com>
2023-04-21 03:42:13 +0000

    Clear results without page refresh

--- scraper/static/hideTable.js
+++ scraper/static/hideTable.js
@@ -1,3 +1,13 @@
+function clearTable() {
+    let resultTable = document.getElementById("result-table");
+    resultTable.innerHTML = "";
+    document.getElementById("emptyMessage").style.display = "block";
+    // Hide the table header, search bar, and clear button
+    document.getElementById('table-header').style.display = 'none';
+    document.getElementById('searchBar').style.display = 'none';
+    document.getElementById('clearResultsBtn').style.display = 'none';
+}
+
 document.addEventListener('DOMContentLoaded', function() {
     // Hide the table header, search bar, and clear button if there are no search results
     let resultTable = document.getElementById('result-table');

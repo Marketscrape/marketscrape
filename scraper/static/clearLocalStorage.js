9427ed76b33309c34d93cc1a2496216f20823a68
alexwholland <alexwholland@gmail.com>
2023-04-21 03:53:14 +0000

    Removed uneeded alert banner

--- scraper/static/clearLocalStorage.js
+++ scraper/static/clearLocalStorage.js
@@ -2,13 +2,4 @@ document.getElementById('clearResultsBtn').addEventListener('click', function() 
     if (localStorage.length > 0) {
         localStorage.clear();
     }
-
-    // Show the Bootstrap alert
-    var alertElement = document.getElementById('clearAlert');
-    // Set the display property to 'block' to make the alert visible
-    alertElement.style.display = 'block';
-
-    document.getElementById('closeAlertBtn').addEventListener('click', function() {
-        alertElement.style.display = 'none';
-    });
 });
